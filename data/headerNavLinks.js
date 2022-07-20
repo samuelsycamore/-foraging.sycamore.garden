b85ed872e25f8b397e92b80d69c76bf6cdd88dc0
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-07-19 21:07:12 -0500

    trailing slashes

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: 'https://tech.sycamore.garden', title: 'Tech' },
+  { href: 'https://tech.sycamore.garden/', title: 'Tech' },
   { href: '/', title: 'Foraging' },
-  { href: 'https://travel.sycamore.garden', title: 'Travel' },
-  { href: 'https://sycamore.garden/music', title: 'Music' },
-  { href: 'https://shop.sycamore.garden', title: 'Shop' },
+  { href: 'https://travel.sycamore.garden/', title: 'Travel' },
+  { href: 'https://sycamore.garden/music/', title: 'Music' },
+  { href: 'https://shop.sycamore.garden/', title: 'Shop' },
   { href: 'https://work.sycamore.garden/', title: 'Work' },
   { href: 'https://sycamore.garden/', title: 'About' },
 ]
