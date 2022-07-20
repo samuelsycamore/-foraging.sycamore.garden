62944e391868266f616a01673b60ecf1ef8b9a06
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-07-19 21:03:08 -0500

    header nav links

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,10 +1,11 @@
 const headerNavLinks = [
   { href: 'https://tech.sycamore.garden', title: 'Tech' },
   { href: '/', title: 'Foraging' },
+  { href: 'https://travel.sycamore.garden', title: 'Travel' },
   { href: 'https://sycamore.garden/music', title: 'Music' },
   { href: 'https://shop.sycamore.garden', title: 'Shop' },
   { href: 'https://work.sycamore.garden/', title: 'Work' },
-  { href: 'https://sycamore.garden/about', title: 'About' },
+  { href: 'https://sycamore.garden/', title: 'About' },
 ]
 
 export default headerNavLinks
