d2198d6167261edc19071289a23c78b0b9f37a0c
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-07-23 17:00:42 -0500

    newsletter stuff

---
+++ layouts/PageLayout.js
@@ -0,0 +1,37 @@
+import Link from '@/components/Link'
+import PageTitle from '@/components/PageTitle'
+import SectionContainer from '@/components/SectionContainer'
+import { BlogSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+
+export default function PostLayout({ frontMatter, authorDetails, children }) {
+  const { slug, title } = frontMatter
+
+  return (
+    <SectionContainer>
+      <BlogSEO
+        url={`${siteMetadata.siteUrl}/plants/${slug}`}
+        authorDetails={authorDetails}
+        {...frontMatter}
+      />
+      <ScrollTopAndComment />
+      <article className="">
+        <div className="m-auto max-w-2xl xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+          <header className="pt-6 xl:pb-6">
+            <div className="space-y-1 text-center">
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
+            </div>
+          </header>
+          <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700">
+            <div className="divide-y divide-gray-200 dark:divide-gray-700">
+              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </SectionContainer>
+  )
+}
