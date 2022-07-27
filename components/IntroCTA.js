6e19bdeee5d804aec23fac7e79f5c4b92edc4f4b
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2022-07-27 10:34:27 -0500

    purslane, intro book cta, seo tweaks

---
+++ components/IntroCTA.js
@@ -0,0 +1,30 @@
+const IntroCTA = () => (
+  <div className="p4">
+    <h2>Introduction to Foraging</h2>
+    <h4>Did you find this article helpful?</h4>
+    <p>
+      This is an excerpt from{' '}
+      <a href="https://shop.sycamore.garden/l/introduction-to-foraging/">
+        <em>
+          Introduction to Foraging: A Beginner's Guide to Gathering Wild Foods With Confidence
+        </em>
+      </a>
+      .
+    </p>
+    <p>
+      Diving into the wonderful world of wild foods can be intimidating, especially if you don't
+      have much experience working with plants.
+    </p>
+    <p>
+      <em>Introduction to Foraging</em> was written with the absolute beginner in mind, and with the
+      goal of getting you out into the world gathering wild foods right away.
+    </p>
+    <p>
+      To learn everything you need to know to begin foraging safely, sustainably, and confidently,{' '}
+      <a href="https://shop.sycamore.garden/l/introduction-to-foraging/">check out the book here</a>
+      . Happy hunting!
+    </p>
+  </div>
+)
+
+export default IntroCTA
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,10 +7,10 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://foraging.sycamore.garden',
-  siteRepo: 'https://github.com/samuelsycamore/garden',
+  siteRepo: 'https://github.com/samuelsycamore/-foraging-sycamore-garden',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/sycamore-400.jpeg',
-  email: 'sam@sycamore.design',
+  email: 'sam@sycamore.garden',
   github: 'https://github.com/samuelsycamore',
   twitter: 'https://twitter.com/tanoaksam',
   linkedin: 'https://www.linkedin.com/in/samuelsycamore',
