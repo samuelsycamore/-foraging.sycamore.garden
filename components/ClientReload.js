853a1cb25fe76202c031d6cd81f32b3188e84028
Sam Sycamore <71297412+samuelsycamore@users.noreply.github.com>
2024-09-11 13:18:38 -0700

    fix busted hot reloader

---
+++ components/ClientReload.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import Router from 'next/router'
+
+/**
+ * Client-side complement to next-remote-watch
+ * Re-triggers getStaticProps when watched mdx files change
+ *
+ */
+export const ClientReload = () => {
+  return null
+}
